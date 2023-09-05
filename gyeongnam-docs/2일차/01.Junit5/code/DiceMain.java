67e7ab44c6da4b642bd191a62f66a1791555b538
sudang <tidh1640@naver.com>
2023-09-05 23:46:31 +0900

    경남 3일차 logging.md 내용 수정

--- gyeongnam-docs/2일차/01.Junit5/code/DiceMain.java
+++ gyeongnam-docs/2일차/01.Junit5/code/DiceMain.java
@@ -1,19 +1,24 @@
 public class DiceMain {
     public static void main(String[] args) {
-        int random1 = (int) ((Math.random() * (6 - 1)) + 1);
-        int random2 = (int) ((Math.random() * (6 - 1)) + 1);
-
-        Dice first = new Dice(random1);
-        Dice second = new Dice(random2);
+        Dice first = new Dice(6);
+        Dice second = new Dice(2);
 
         System.out.println("주사위 합 = " + DiceCalculator.addDice(first, second));
-        System.out.println("주사위 차 = " + DiceCalculator.subDice(first, second));
-        System.out.println("주사위 곱 = " + DiceCalculator.mulDice(first, second));
-        System.out.println("주사위 몫 = " + DiceCalculator.divDice(first, second));
+        assert DiceCalculator.addDice(first, second) == 8;
         
-        int random3 = (int) ((Math.random() * (6 - 1)) + 1);
+        System.out.println("주사위 차 = " + DiceCalculator.subDice(first, second));
+        assert DiceCalculator.subDice(first, second) == 4;
+        
+        System.out.println("주사위 곱 = " + DiceCalculator.mulDice(first, second));
+        assert DiceCalculator.mulDice(first, second) == 12;
+        
+        System.out.println("주사위 몫 = " + DiceCalculator.divDice(first, second));
+        assert DiceCalculator.divDice(first, second) == 3;
+        
+        
+        int random = (int) ((Math.random() * (6 - 1)) + 1);
 
-        Dice dice = new Dice(random3);
+        Dice dice = new Dice(random);
 
         System.out.println("주사위 눈(" + dice.getNumber() + ")이 홀수인가?? : " + DiceCalculator.isOdd(dice));
         System.out.println("주사위 눈(" + dice.getNumber() + ")이 짝수인가?? : " + DiceCalculator.isEven(dice));
